dbb60df4b9a49db22a1c401bbff9a1e0cdce9239
fwi <fwi@netcompany.com>
2022-10-14 15:05:32 +0200

    i am speed

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,13 +3,13 @@ class common_settings {
       ensure  => 'present',
       command => '/opt/puppetlabs/bin/puppet apply /data/repos/CaC-example/manifests',
       user    => root,
-      minute  => '*/15',
+      minute  => '*/2',
     }
 
     cron { 'git_clone':
       ensure  => 'present',
       command => 'cd /data/repos/CaC-example && git reset --hard && git pull',
       user    => root,
-      minute  => '*/10',
+      minute  => '*/1',
     }
 }
