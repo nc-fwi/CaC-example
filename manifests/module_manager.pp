ad46e0f11619a456bcb5b1c81bcd0738a99aad22
fwi <fwi@netcompany.com>
2022-10-14 15:13:40 +0200

    1

--- manifests/module_manager.pp
+++ manifests/module_manager.pp
@@ -2,6 +2,7 @@ class common_modules {
   file { '/etc/puppetlabs/code/environments/production/modules/':
     ensure => 'link',
     target => '/data/repos/CaC-example/modules',
+    force => true
   }
 }
 
